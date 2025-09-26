a36d9596b8f31292c584491057f6fce26dbaea8e
kerstinlundgren <135594145+kerstinlundgren@users.noreply.github.com>
2025-09-26 17:49:00 +0200

    Update export_confluence.sh

--- export_confluence.sh
+++ export_confluence.sh
@@ -5,7 +5,7 @@ TMP="./tmp_export"
 rm -rf "$DEST" "$TMP"
 mkdir -p "$TMP" "$DEST"
 curl -s -u "$CONFLUENCE_USER:$CONFLUENCE_TOKEN" \
-  "$CONFLUENCE_BASE/rest/api/space/$CONFLUENCE_SPACE/export?exportType=html" \
+  "$CONFLUENCE_BASE/rest/api/space/$CONFLUENCE_SPACE/export" \
   -o "$TMP/manual.zip"
 unzip -q "$TMP/manual.zip" -d "$DEST"
 echo "User-agent: *" > "$DEST/robots.txt"
